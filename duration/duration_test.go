1c0c40695e021e1d74f4bc6d9b80b36ba0ee27f1
msaf1980 <msaf1980@gmail.com>
2022-12-31 20:05:24 +0500

    time.Duration short String

---
+++ duration/duration_test.go
@@ -0,0 +1,49 @@
+package duration
+
+import (
+	"testing"
+	"time"
+)
+
+func TestString(t *testing.T) {
+	tests := []struct {
+		d    time.Duration
+		want string
+	}{
+		{
+			d:    time.Hour,
+			want: "1h",
+		},
+		{
+			d:    12*time.Hour + time.Second,
+			want: "12h1s",
+		},
+		{
+			d:    12*time.Hour + 31*time.Minute + 7*time.Second,
+			want: "12h31m7s",
+		},
+		{
+			d:    12*time.Hour + 31*time.Minute + 7*time.Second + 100*time.Millisecond + time.Nanosecond,
+			want: "12h31m7.100000001s",
+		},
+		{
+			d:    12*time.Hour + 31*time.Minute + 7*time.Second + 10*time.Millisecond + time.Nanosecond,
+			want: "12h31m7.010000001s",
+		},
+		{
+			d:    12*time.Hour + 31*time.Minute + 7*time.Second + time.Microsecond + 10*time.Nanosecond,
+			want: "12h31m7.00000101s",
+		},
+		{
+			d:    12*time.Hour + 31*time.Minute + 10*time.Nanosecond,
+			want: "12h31m0.00000001s",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.d.String(), func(t *testing.T) {
+			if got := String(tt.d); got != tt.want {
+				t.Errorf("String() = %v, want %v (%s)", got, tt.want, tt.d.String())
+			}
+		})
+	}
+}
